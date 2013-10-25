d809da042ea717f4221a92a9d321b640900c885f
itsmartin <github@martinbrook.com>
2013-10-25 15:25:24 +0000

    Modifications for 1.7 to start up cleanly

--- mk2/events/server.py
+++ mk2/events/server.py
@@ -4,7 +4,7 @@ from . import Event, get_timestamp
 
 # input/output
 output_exp = re.compile(
-    r'(\d{4}-\d{2}-\d{2} |)(\d{2}:\d{2}:\d{2}) \[([A-Z]+)\] (?:%s)?(.*)' % '|'.join((re.escape(x) for x in (
+    r'\[(\d{2}:\d{2}:\d{2})\] \[Server thread/([A-Z]+)\]: (?:%s)?(.*)' % '|'.join((re.escape(x) for x in (
         '[Minecraft] ',
         '[Minecraft-Server] '
     ))))
@@ -30,9 +30,9 @@ class ServerOutput(Event):
         m = output_exp.match(self.line)
         if m:
             g = m.groups()
-            self.time = g[0]+g[1]
-            self.level= g[2]
-            self.data = g[3]
+            self.time = g[0]
+            self.level= g[1]
+            self.data = g[2]
         else:
             self.level= "???"
             self.data = self.line.strip()
